e4c19ccf87cdadbfd9491195375df950badf8d39
sbin0819 <sbinha123@gmail.com>
2021-10-22 14:41:45 +0900

    ENH: 기본세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from '../styles/globalStyle';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>boilerplate</title>
+      </Head>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
-export default MyApp
+
+export default MyApp;
