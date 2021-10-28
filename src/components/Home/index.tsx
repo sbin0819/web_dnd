78075310a8b778700022524ef6dcfe0ca384eb6f
sbin0819 <sbinha123@gmail.com>
2021-10-28 17:32:31 +0900

    ENH: scroll

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -6,30 +6,40 @@ import Searchbar from '@components/Searchbar';
 import Card from '@common/Card';
 
 import useWindowSize from '@hooks/useWindowSize';
-
+import useScoll from '@hooks/useScroll';
 const Container = styled.div`
-  position: relative;
   display: flex;
   padding: 50px 35px;
+  overflow: hidden;
 `;
 
-const SidebarContainer = styled.div`
+interface IScroll {
+  isScroll: boolean;
+}
+
+const SidebarContainer = styled.div<IScroll>`
+  position: relative;
+  top: ${({ isScroll }) => isScroll && '80px'};
   width: 300px;
   height: 100px;
 `;
 
-const CardContainer = styled.div`
+const CardContainer = styled.div<IScroll>`
   flex: 3;
   display: flex;
   gap: 20px;
   flex-wrap: wrap;
   justify-content: space-between;
   margin: 0 5px;
+  height: 100vh;
+  overflow: ${({ isScroll }) => isScroll && 'scroll'};
 `;
 
 function Home() {
+  const [isScroll, setIsScroll] = useState(false);
   const [isSidebar, setIsSidebar] = useState(false);
   const { width } = useWindowSize();
+  const { scrollPosition } = useScoll();
   useEffect(() => {
     if (width && width > 800) {
       setIsSidebar(true);
@@ -37,19 +47,26 @@ function Home() {
       setIsSidebar(false);
     }
   }, [width]);
+  useEffect(() => {
+    if (scrollPosition > 150) {
+      setIsScroll(true);
+    } else {
+      setIsScroll(false);
+    }
+  }, [scrollPosition]);
   return (
     <div>
       <Searchbar />
       <Container>
         {isSidebar && (
-          <SidebarContainer>
+          <SidebarContainer isScroll={isScroll}>
             <div className="sticky">
               <Favorite />
             </div>
           </SidebarContainer>
         )}
-        <CardContainer>
-          {[0, 0, 0, 0, 0, 0, 0, 0, 0].map((_, i) => (
+        <CardContainer isScroll={isScroll}>
+          {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((_, i) => (
             <Card key={i} />
           ))}
         </CardContainer>
